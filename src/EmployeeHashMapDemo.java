0132f3320c0fafcb9f69cc51a75fd13935faf96b
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-14 15:55:38 +0400

    add Task 1: Create and Add Elements to a Java HashMap

---
+++ src/EmployeeHashMapDemo.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmployeeHashMapDemo {
+    public static void main(String[] args){
+        HashMap<Integer, String> employeeIDname = new HashMap<>();
+        employeeIDname.put(1,"Ahmed"); // adding keyes and value to HashMap
+        employeeIDname.put(2,"Sara");
+        employeeIDname.put(5,"Hoor");
+        employeeIDname.put(4,"Noor");
+        employeeIDname.put(7,"Muna");
+        for(Map.Entry<Integer,String> result : employeeIDname.entrySet()){ // printing HashMap
+            System.out.println(result);
+        }
+        employeeIDname.put(1,"Ali"); // add another value to same key (update)
+        for(Map.Entry<Integer,String> result : employeeIDname.entrySet()){
+            System.out.println(result);
+        }
+    }
+}
